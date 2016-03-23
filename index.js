ccae0f4d1e8d31b7bd836d4b84e6f7e030f39277
Brian Woodward <brian.woodward@gmail.com>
2016-03-23 18:08:05 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * module-dependents <https://github.com/doowb/module-dependents>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
